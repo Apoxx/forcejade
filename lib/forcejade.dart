0980d7956798fc3d9f0caa79813358df856e1d36
Apox <xzzy159@gmail.com>
2014-03-31 23:19:51 +0200

    Adds devMode

--- lib/forcejade.dart
+++ lib/forcejade.dart
@@ -5,59 +5,71 @@ import "dart:async";
 import "package:forcemvc/force_mvc.dart";
 import "dart:isolate";
 import "package:jaded/jaded.dart" as jade;
+import "package:isolate_pool/isolate_pool.dart";
 
 class JadeRender extends ForceViewRender{
   String viewDir;
-  bool onFlyCompile;
-  SendPort _sPort;
-  ReceivePort _rPort;
+  bool devMode;
+  IsolateWorker child;
   int idCpt;
+  IsolatePool proxy;
   Map<int,Completer> completers;
 
-  JadeRender({this.viewDir :"../views"}) : super(){
-    _sPort = null;
-    _rPort = null;
+  JadeRender({this.viewDir :"../views", this.devMode : false}) : super(){
     idCpt = 0;
+    proxy = null;
     completers = new Map<int,Completer>();
-    _rPort = new ReceivePort();
     runIsolate();
   }
 
   Future<String> render(String view, model) {
     Completer<String> comp = new Completer<String>();
-    String filePath = "$viewDir$view.jade".replaceAll("\\","/");
-    completers[idCpt] = comp;
-    _sPort.send([idCpt,filePath,model]);
-    idCpt++;
+    var rend = (_){
+      String filePath = "$viewDir$view.jade".replaceAll("\\","/");
+      completers[idCpt] = comp;
+      child.send({"idCpt": idCpt,"filePath": filePath,"model": model});
+      idCpt++;
+    };
+    if(devMode)
+      runIsolate().then(rend);
+    else
+      rend();
     return comp.future;
   }
 
-  void runIsolate() {
+  Future runIsolate() {
+    Completer comp = new Completer();
     compile(viewDir : this.viewDir);
-    Isolate.spawnUri(new Uri.file("$viewDir/jaded.views.dart"), [], _rPort.sendPort);
-    _rPort.listen((message){
-      if(_sPort == null){
-        _sPort = message;
-      }else{
-        completers[message[0]].complete(message[1]);
-        completers.remove(message[0]);
-      }
+    if(proxy != null){
+      completers.forEach((k,v) => completers[k].complete(""));
+      completers.clear();
+      proxy.close();
+      proxy = null;
+    }
+    proxy = new IsolatePool();
+    proxy.runIsolate(new Uri.file("$viewDir/jaded.views.dart"),[]).then((IsolateWorker child){
+      this.child = child;
+      child.stream.listen((IsolateMessage data){
+        completers[data.message["idCpt"]].complete(data.message["html"]);
+        completers.remove(data.message["idCpt"]);
+      });
+      comp.complete(true);
     });
-
+    return comp.future;
   }
 
   static void compile({String viewDir : "../views"}){
     var jadeTemplates = jade.renderDirectory(viewDir);
-    jadeTemplates = jadeTemplates.replaceFirst("\n","\nimport 'dart:isolate';\n");
+    jadeTemplates = jadeTemplates.replaceFirst("\n",'\nimport "package:isolate_pool/isolate_pool.dart";\n');
     var isolateWrapper =
     """
   $jadeTemplates
 
   main(List args, SendPort replyTo) {
-    ReceivePort rPort = new ReceivePort();
-    replyTo.send(rPort.sendPort);
-    rPort.listen((value){
-      replyTo.send([value[0],JADE_TEMPLATES[value[1]](value[2])]);
+    var mirror = new IsolateWorker.isolateInit(replyTo);
+    mirror.stream.listen((IsolateMessage msg){
+      var value = msg.message;
+      msg.source.send({"idCpt" : value["idCpt"],"html" : JADE_TEMPLATES[value["filePath"]](value["model"])});
     });
   }
   """;
